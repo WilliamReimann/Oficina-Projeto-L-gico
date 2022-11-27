3f99fbbac03a794b3daf6b1dc2c1785deff1a666
WilliamReimann <99360427+WilliamReimann@users.noreply.github.com>
2022-11-27 00:32:16 -0300

    Correção

--- Oficina - Persistência de Dados.sql
+++ Oficina - Persistência de Dados.sql
@@ -13,41 +13,41 @@ insert into Cliente (Nome, CPF, Contato)
           ('Anthony', '78901234567','55989012345');
           
 
-insert into Veículo(Modelo, Revisão/Reparo, Liberado ) 
-values     ('Renault Kwid Zen 1.0 flex',Revisão,Liberado),
-           ('Peugeot 208 Like 1.0',Revisão,null),
-           ('Chevrolet Onix Plus LT 1.0 flex',Reparo,Liberado,null),
-	       ('Chevrolet Onix 1.0 flex',Reparo,null),
-           ('Fiat Argo Drive 1.0 flex',Revisão,Liberado),
-           ('Hyundai HB20 Platinum 1.0 turbo flex',Reparo,Liberado);
+insert into Veículo (Modelo, Revisão/Reparo, Liberado) 
+values     ('Renault Kwid Zen 1.0 flex','Revisão',true),
+           ('Peugeot 208 Like 1.0','Revisão',false),
+           ('Chevrolet Onix Plus LT 1.0 flex','Reparo',true),
+	       ('Chevrolet Onix 1.0 flex','Reparo',true),
+           ('Fiat Argo Drive 1.0 flex','Revisão',true),
+           ('Hyundai HB20 Platinum 1.0 turbo flex','Reparo',false);
       
 
     insert into Mecânico(Código, Nome, Endereço, Especialidade)
-      values (1,Joseph Junior,Vila Olimpia 2050, Reparo),
-             (2,Morgan Freeman,Alphville 3070, Revisão);
+      values (1,'Joseph Junior','Vila Olimpia 2050', 'Reparo'),
+             (2,'Morgan Freeman','Alphville 3070', 'Revisão');
 			
              
              
-	  insert into Ordem de Serviço(Número, Situação, Emissão, Entrega, Valor Mão de Obra, Valor da Peça)
-             values (1,Pronto,26/11/2022,26/11/2022,500),
-					(2,Pronto,26/11/2022,26/11/2022,200),
-                    (3,Espera,26/11/2022,null,500),
-                    (4,Espera,26/11/2022,null,200),
-                    (5,Reparando,26/11/2022,null,500),
-                    (6,Revisão,26/11/2022,null,200);
+	  insert into OrdemDeServiço(Número, Situação, Emissão, Entrega, Valor Mão de Obra, Valor da Peça)
+             values (1,Pronto,'26/11/2022','26/11/2022','500.00'),
+					(2,Pronto,'26/11/2022','26/11/2022','200.00'),
+                    (3,Espera,'26/11/2022',null,'500.00'),
+                    (4,Espera,'26/11/2022',null,'200.00'),
+                    (5,Reparando,'26/11/2022',null,'500.00'),
+                    (6,Revisão,'26/11/2022',null,'200.00');
        
       
     
        
        
        
-       -- Relação de mecânicos e veículos:
-select idProduct, Fname, SocialName, CNPJ from product
-		inner join product_supplier on idProduct=idSProduct
-        inner join supplier on idSupplier = idSSupllier;
+       -- Relação de veículos e mecânicos:
+select Código, Modelo, Revisão/Reparo, Liberado from Mecânico
+		inner join Veículo_Mecânico on idVeículo=idMVeículo
+        inner join Mecânico on idMecânico = idMecânico;
         
-         -- Mostrar o nome e preço dos serviços com preço maior que 200 reais
-select Fname, Category, Price from product having Price > 200 order by Fname, Category;
+         -- Mostrar a situação dos serviços com preço maior que 200 reais
+select Número, Situação, Valor from OrdemDeServiço having Valor > 200 order by Número, Situação;
 
        -- Quantos veículos cadastrados têm o modelo igual Chevrolet?
 select count(*) from Veículos where Modelo = 'Chevrolet';
